797e8304c3cc53b29b6c72b57dd88b11c3ab063d
USUARIO <USUARIO@CECILIACORTES>
2016-09-12 08:11:13 -0500

    mmm

--- INEQ2/INEQ2/Controllers/EquipmentsController.cs
+++ INEQ2/INEQ2/Controllers/EquipmentsController.cs
@@ -1,113 +1,113 @@
 ﻿using System.Data.Entity;
-using System.Threading.Tasks;
 using System.Net;
 using System.Web.Mvc;
 using INEQ2.Models;
+using System.Threading.Tasks;
 using INEQ2.DAL;
 
 namespace INEQ2.Controllers
 {
-    public class BrandsController : Controller
+    public class EquipmentsController : Controller
     {
-        private DAL db = new DAL();
+        private DAL.DAL db = new DAL.DAL();
 
-        // GET: Brands
-        public async Task<ActionResult> Index()
+        // GET: Equipments
+        public ActionResult Index()
         {
-            return View(await db.Brand.ToListAsync());
+            return View(db.Equipments.ToListAsync());
         }
 
-        // GET: Brands/Details/5
+        // GET: Equipments/Details/5
         public async Task<ActionResult> Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Brand brand = await db.Brand.FindAsync(id);
-            if (brand == null)
+            Equipment equipment = await db.Equipments.FindAsync(id);
+            if (equipment == null)
             {
                 return HttpNotFound();
             }
-            return View(brand);
+            return View(equipment);
         }
 
-        // GET: Brands/Create
+        // GET: Equipments/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Brands/Create
+        // POST: Equipments/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include = "ID,Description,Active")] Brand brand)
+        public async Task<ActionResult> Create([Bind(Include = "Id,EquipmentTypeId,ModelId,BrandId,StatusId,WarehouseId,EntryDate,Serie,SofttekId,Active")] Equipment equipment)
         {
             if (ModelState.IsValid)
             {
-                db.Brand.Add(brand);
+                db.Equipments.Add(equipment);
                 await db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
 
-            return View(brand);
+            return View(equipment);
         }
 
-        // GET: Brands/Edit/5
+        // GET: Equipments/Edit/5
         public async Task<ActionResult> Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Brand brand = await db.Brand.FindAsync(id);
-            if (brand == null)
+            Equipment equipment = await db.Equipments.FindAsync(id);
+            if (equipment == null)
             {
                 return HttpNotFound();
             }
-            return View(brand);
+            return View(equipment);
         }
 
-        // POST: Brands/Edit/5
+        // POST: Equipments/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include = "ID,Description,Active")] Brand brand)
+        public async Task<ActionResult> Edit([Bind(Include = "Id,EquipmentTypeId,ModelId,BrandId,StatusId,WarehouseId,EntryDate,Serie,SofttekId,Active")] Equipment equipment)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(brand).State = EntityState.Modified;
+                db.Entry(equipment).State = EntityState.Modified;
                 await db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
-            return View(brand);
+            return View(equipment);
         }
 
-        // GET: Brands/Delete/5
+        // GET: Equipments/Delete/5
         public async Task<ActionResult> Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Brand brand = await db.Brand.FindAsync(id);
-            if (brand == null)
+            Equipment equipment = await db.Equipments.FindAsync(id);
+            if (equipment == null)
             {
                 return HttpNotFound();
             }
-            return View(brand);
+            return View(equipment);
         }
 
-        // POST: Brands/Delete/5
+        // POST: Equipments/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> DeleteConfirmed(int id)
         {
-            Brand brand = await db.Brand.FindAsync(id);
-            db.Brand.Remove(brand);
+            Equipment equipment = await db.Equipments.FindAsync(id);
+            db.Equipments.Remove(equipment);
             await db.SaveChangesAsync();
             return RedirectToAction("Index");
         }
